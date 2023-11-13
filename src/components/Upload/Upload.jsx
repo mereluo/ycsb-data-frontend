73c50ff45c788d11165dac57b419ee7dd4b0452e
mereluo <meredithluo710@gmail.com>
2023-11-13 13:58:46 -0800

    Call API to create workloadA

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -62,6 +62,81 @@ function Upload() {
                 },
             });
         }
+        const option = createWorkloadA("MongoDB");
+    };
+
+    const createWorkloadA = async (name) => {
+        try {
+            // Create DatabaseOption
+            const databaseOptionResponse = await fetch("http://localhost:8080/api/dbOption", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ database: name }),
+            });
+            const databaseOption = await databaseOptionResponse.json();
+            console.log(databaseOption);
+
+            // Create DBconfig
+            const dbConfigResponse = await fetch("http://localhost:8080/api/dbConfig", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    databaseOption: { id: databaseOption.id },
+                    description: "Test DBConfig",
+                    isTransactional: true,
+                    platform: "Test Platform",
+                    numOfNodes: 3,
+                    numOfRegions: 2,
+                    isMultiRegion: true,
+                }),
+            });
+            const dbConfig = await dbConfigResponse.json();
+            console.log(dbConfig + " " + dbConfig.id);
+
+            // Create TestConfig
+            const testConfigResponse = await fetch("http://localhost:8080/api/testConfig", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    concurrencyLevel: 64,
+                    recordCounts: 3,
+                    commandLine: "test commandLine",
+                    dbConfig: { id: dbConfig.id },
+                }),
+            });
+            const testConfig = await testConfigResponse.json();
+
+            // Create WorkloadA
+            const workloadAResponse = await fetch("http://localhost:8080/api/workloadA", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    opsPerSec: 100.0,
+                    readMeanLatency: 10.5,
+                    readMaxLatency: 20.5,
+                    readP95: 15.5,
+                    readP99: 25.5,
+                    updateMeanLatency: 8.5,
+                    updateMaxLatency: 18.5,
+                    updateP95: 13.5,
+                    updateP99: 23.5,
+                    timeSeries: { key: "testkey", value: "testlatency" },
+                    testConfigA: { id: testConfig.id },
+                }),
+            });
+            const workloadA = await workloadAResponse.json();
+            console.log("WorloadA created: ", workloadA);
+        } catch (error) {
+            console.error("Error creating workloadA: ", error);
+        }
     };
 
     return (
