247ae369999c811cc5ff4abcf9bbc9f01320147b
mereluo <meredithluo710@gmail.com>
2024-01-11 16:57:28 -0800

    Add state object and handle input func

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -7,8 +7,26 @@ function Questions() {
         { id: 3, label: "MongoDB" },
     ];
 
-    const [question1Response, setQuestion1Response] = useState(null);
+    const [formState, setFormState] = useState({
+        concurrencyLevel: null,
+        recordCounts: null,
+        commandLine: "",
+        isTransactional: null,
+        platform: "",
+        numOfNodes: null,
+        isMultiRegion: null,
+        numOfRegions: null,
+        description: "",
+        database: "",
+    });
 
+    const handleInputChange = (fieldName, value) => {
+        setFormState((prevState) => ({ ...prevState, [fieldName]: value }));
+    };
+
+    const handleSubmit = () => {
+        console.log(formState);
+    };
     return (
         <div>
             <div className="card text-center">
@@ -20,8 +38,10 @@ function Questions() {
                     <div className="form-check">
                         {DBOption.map((item) => (
                             <div key={item.id}>
-                                <input className="form-check-input" type="checkbox" />
-                                <label className="form-check-label">{item.label}</label>
+                                <input className="form-check-input" type="radio" name="databaseOptions" id={`databaseOption${item.id}`} value={item.label} onChange={() => handleInputChange("database", item.label)} />
+                                <label className="form-check-label" htmlFor={`databaseOption${item.id}`}>
+                                    {item.label}
+                                </label>
                             </div>
                         ))}
                     </div>
@@ -34,36 +54,42 @@ function Questions() {
                 <div className="card-body">
                     <h5 className="card-title">Please indicate the database configuration in the following field</h5>
 
+                    <p className="card-text">Description</p>
+                    <input type="text" id="description-input" className="form-control col-3" onChange={(e) => handleInputChange("description", e.target.value)} />
+
+                    <p className="card-text mt-2">Platform</p>
+                    <input type="text" id="platform-input" className="form-control col-3" onChange={(e) => handleInputChange("platform", e.target.value)} />
+
                     <p className="card-text">Is Transactional</p>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" onChange={() => handleInputChange("isTransactional", true)} />
                         <label className="form-check-label" htmlFor="inlineRadio1">
                             Yes (YCSB+T)
                         </label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" onChange={() => handleInputChange("isTransactional", false)} />
                         <label className="form-check-label" htmlFor="inlineRadio2">
                             No
                         </label>
                     </div>
 
                     <p className="card-text">Number of Nodes</p>
-                    <input type="number" id="number-input" className="form-control " />
+                    <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("numOfNodes", e.target.value)} />
 
                     <p className="card-text">Is MultiRegion</p>
 
-                    <button className="btn btn-primary" onClick={() => setQuestion1Response("yes")}>
+                    <button className="btn btn-primary" onClick={() => handleInputChange("isMultiRegion", true)}>
                         Yes
                     </button>
-                    <button className="btn btn-primary" style={{ marginLeft: "5px" }} onClick={() => setQuestion1Response("no")}>
+                    <button className="btn btn-primary" style={{ marginLeft: "5px" }} onClick={() => handleInputChange("isMultiRegion", false)}>
                         No
                     </button>
 
-                    {question1Response === "yes" && (
+                    {formState.isMultiRegion && (
                         <div>
                             <p className="card-text">Number of regions</p>
-                            <input type="number" id="number-input" className="form-control " />
+                            <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("numOfRegions", e.target.value)} />
                         </div>
                     )}
                 </div>
@@ -76,26 +102,29 @@ function Questions() {
                     <h5 className="card-title">Please indicate the database configuration in the following field</h5>
                     <p className="card-text">Concurrency Level</p>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio1" value="option1" />
+                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio1" value="option1" onChange={() => handleInputChange("concurrencyLevel", 64)} />
                         <label className="form-check-label" htmlFor="inlineRadio1">
                             64
                         </label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio2" value="option2" />
+                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio2" value="option2" onChange={() => handleInputChange("concurrencyLevel", 128)} />
                         <label className="form-check-label" htmlFor="inlineRadio2">
                             128
                         </label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio3" value="option3" />
+                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio3" value="option3" onChange={() => handleInputChange("concurrencyLevel", 256)} />
                         <label className="form-check-label" htmlFor="inlineRadio2">
                             256
                         </label>
                     </div>
 
                     <p className="card-text">Record Counts</p>
-                    <input type="number" id="number-input" className="form-control " />
+                    <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("recordCounts", e.target.value)} />
+
+                    <p className="card-text">Command Line</p>
+                    <input type="text" id="command-line-input" className="form-control col-3" onChange={(e) => handleInputChange("commandLine", e.target.value)} />
                 </div>
             </div>
 
@@ -123,6 +152,11 @@ function Questions() {
                         </label>
                     </div>
                 </div>
+            </div>
+            <div className="mt-2 text-center">
+                <button className="btn btn-primary" onClick={handleSubmit}>
+                    Submit
+                </button>
             </div>
         </div>
     );
