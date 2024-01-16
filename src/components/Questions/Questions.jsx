f0f4200924ea7f14b9a3e69f221c8e2a7eeb2143
mereluo <meredithluo710@gmail.com>
2024-01-16 15:21:14 -0800

    Add more styling

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -23,6 +23,7 @@ function Questions() {
         database: "",
     });
 
+
     const handleInputChange = (fieldName, value) => {
         setFormState((prevState) => ({ ...prevState, [fieldName]: value }));
     };
@@ -47,133 +48,141 @@ function Questions() {
     };
 
     return (
-        <div>
-            <div className="card text-center">
-                <div className="card-header">Database Options</div>
+        <div className="question-container mt-2">
+            <div className="row">
+                <div className="card text-center col mr-3">
+                    <div className="card-header">Database Options</div>
 
-                <div className="card-body">
-                    <h5 className="card-title">Please select the database that you want to do the comparison</h5>
-
-                    <div className="form-check">
-                        {DBOption.map((item) => (
-                            <div key={item.id}>
-                                <input className="form-check-input" type="radio" name="databaseOptions" id={`databaseOption${item.id}`} value={item.label} onChange={() => handleInputChange("database", item.label)} />
-                                <label className="form-check-label" htmlFor={`databaseOption${item.id}`}>
-                                    {item.label}
-                                </label>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-            <div className="card text-center">
-                <div className="card-header">Database Configuration</div>
-
-                <div className="card-body">
-                    <h5 className="card-title">Please indicate the database configuration in the following field</h5>
-
-                    <p className="card-text">Description</p>
-                    <input type="text" id="description-input" className="form-control col-3" onChange={(e) => handleInputChange("description", e.target.value)} />
-
-                    <p className="card-text mt-2">Platform</p>
-                    <input type="text" id="platform-input" className="form-control col-3" onChange={(e) => handleInputChange("platform", e.target.value)} />
-
-                    <p className="card-text">Is Transactional</p>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" onChange={() => handleInputChange("isTransactional", true)} />
-                        <label className="form-check-label" htmlFor="inlineRadio1">
-                            Yes (YCSB+T)
-                        </label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" onChange={() => handleInputChange("isTransactional", false)} />
-                        <label className="form-check-label" htmlFor="inlineRadio2">
-                            No
-                        </label>
-                    </div>
-
-                    <p className="card-text">Number of Nodes</p>
-                    <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("numOfNodes", e.target.value)} />
-
-                    <p className="card-text">Is MultiRegion</p>
-
-                    <button className="btn btn-primary" onClick={() => handleInputChange("isMultiRegion", true)}>
-                        Yes
-                    </button>
-                    <button className="btn btn-primary" style={{ marginLeft: "5px" }} onClick={() => handleInputChange("isMultiRegion", false)}>
-                        No
-                    </button>
-
-                    {formState.isMultiRegion && (
-                        <div>
-                            <p className="card-text">Number of regions</p>
-                            <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("numOfRegions", e.target.value)} />
+                    <div className="card-body">
+                        <div className="form-check">
+                            {DBOption.map((item) => (
+                                <div key={item.id}>
+                                    <input className="form-check-input" type="radio" name="databaseOptions" id={`databaseOption${item.id}`} value={item.label} onChange={() => handleInputChange("database", item.label)} />
+                                    <label className="form-check-label" htmlFor={`databaseOption${item.id}`}>
+                                        {item.label}
+                                    </label>
+                                </div>
+                            ))}
                         </div>
-                    )}
-                </div>
-            </div>
-
-            <div className="card text-center">
-                <div className="card-header">Test Configuration</div>
-
-                <div className="card-body">
-                    <h5 className="card-title">Please indicate the database configuration in the following field</h5>
-                    <p className="card-text">Concurrency Level</p>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio1" value="option1" onChange={() => handleInputChange("concurrencyLevel", 64)} />
-                        <label className="form-check-label" htmlFor="inlineRadio1">
-                            64
-                        </label>
                     </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio2" value="option2" onChange={() => handleInputChange("concurrencyLevel", 128)} />
-                        <label className="form-check-label" htmlFor="inlineRadio2">
-                            128
-                        </label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio3" value="option3" onChange={() => handleInputChange("concurrencyLevel", 256)} />
-                        <label className="form-check-label" htmlFor="inlineRadio2">
-                            256
-                        </label>
-                    </div>
+                    <div className="card-header">Database Configuration</div>
 
-                    <p className="card-text">Record Counts</p>
-                    <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("recordCounts", e.target.value)} />
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col">
+                                <p className="card-text">Description</p>
+                                <input type="text" id="description-input" className="form-control col" onChange={(e) => handleInputChange("description", e.target.value)} />
+                            </div>
+                            <div className="col">
+                                <p className="card-text mt-2">Platform</p>
+                                <input type="text" id="platform-input" className="form-control col" onChange={(e) => handleInputChange("platform", e.target.value)} />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <p className="card-text">Is Transactional</p>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" onChange={() => handleInputChange("isTransactional", true)} />
+                                    <label className="form-check-label" htmlFor="inlineRadio1">
+                                        Yes (YCSB+T)
+                                    </label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" onChange={() => handleInputChange("isTransactional", false)} />
+                                    <label className="form-check-label" htmlFor="inlineRadio2">
+                                        No
+                                    </label>
+                                </div>
+                            </div>
 
-                    <p className="card-text">Command Line</p>
-                    <input type="text" id="command-line-input" className="form-control col-3" onChange={(e) => handleInputChange("commandLine", e.target.value)} />
-                </div>
-            </div>
+                            <div className="col">
+                                <p className="card-text">Number of Nodes</p>
+                                <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("numOfNodes", Math.max(1, e.target.value))} min="1" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <p className="card-text">Is Multi-regional</p>
 
-            <div className="card text-center">
-                <div className="card-header">Workload Option</div>
-
-                <div className="card-body">
-                    <h5 className="card-title">Please indicate the workload you want to test</h5>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="workloadOptions" id="inlineRadio1" value="option1" />
-                        <label className="form-check-label" htmlFor="inlineRadio1">
-                            Workload A
-                        </label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="workloadOptions" id="inlineRadio2" value="option2" />
-                        <label className="form-check-label" htmlFor="inlineRadio2">
-                            Workload B
-                        </label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" name="workloadOptions" id="inlineRadio3" value="option3" />
-                        <label className="form-check-label" htmlFor="inlineRadio2">
-                            Workload F
-                        </label>
+                                <button className="btn btn-light" onClick={() => handleInputChange("isMultiRegion", true)}>
+                                    Yes
+                                </button>
+                                <button className="btn btn-light" style={{ marginLeft: "5px" }} onClick={() => handleInputChange("isMultiRegion", false)}>
+                                    No
+                                </button>
+                            </div>
+                            {formState.isMultiRegion && (
+                                <div className="col">
+                                    <p className="card-text">Number of regions</p>
+                                    <input type="number" id="number-input" className="form-control " onChange={(e) => handleInputChange("numOfRegions", Math.max(1, e.target.value))} min="1" />
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
+
+                <div className="card text-center col">
+                    <div className="card-header">Test Configuration</div>
+
+                    <div className="card-body">
+                        <p className="card-text">Concurrency Level</p>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio1" value="option1" onChange={() => handleInputChange("concurrencyLevel", 64)} />
+                            <label className="form-check-label" htmlFor="inlineRadio1">
+                                64
+                            </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio2" value="option2" onChange={() => handleInputChange("concurrencyLevel", 128)} />
+                            <label className="form-check-label" htmlFor="inlineRadio2">
+                                128
+                            </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="concurrencyOptions" id="inlineRadio3" value="option3" onChange={() => handleInputChange("concurrencyLevel", 256)} />
+                            <label className="form-check-label" htmlFor="inlineRadio2">
+                                256
+                            </label>
+                        </div>
+
+                        <div className="row">
+                            <div className="col">
+                                <p className="card-text">Record Counts</p>
+                                <input type="number" id="number-input" className="form-control" onChange={(e) => handleInputChange("recordCounts", Math.max(1, e.target.value))} min="1" />
+                            </div>
+                            <div className="col">
+                                <p className="card-text">Command Line</p>
+                                <input type="text" id="command-line-input" className="form-control col" onChange={(e) => handleInputChange("commandLine", e.target.value)} />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card-header">Workload Option</div>
+
+                    <div className="card-body">
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="workloadOptions" id="inlineRadio1" value="option1" />
+                            <label className="form-check-label" htmlFor="inlineRadio1">
+                                Workload A
+                            </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="workloadOptions" id="inlineRadio2" value="option2" />
+                            <label className="form-check-label" htmlFor="inlineRadio2">
+                                Workload B
+                            </label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="workloadOptions" id="inlineRadio3" value="option3" />
+                            <label className="form-check-label" htmlFor="inlineRadio2">
+                                Workload F
+                            </label>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="mt-2 text-center">
-                <button className="btn btn-primary" onClick={(event) => handleSubmit(event)}>
+            <div className="mt-3 text-center">
+                <button className="btn btn-outline-primary col-md-5" onClick={(event) => handleSubmit(event)}>
                     Submit
                 </button>
             </div>
--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,52 +1,47 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import "./result.css";
 
 function Result() {
     const location = useLocation();
     const navigateTo = useNavigate();
     const workload = location.state.workload;
-
+    const database = workload.testConfigA.dbConfig.databaseOption.database;
+    console.log(workload);
     const goBack = () => {
         navigateTo("/start");
     };
+    const downloadCSV = () => {
+        // Add logic to download data as CSV
+    };
+    const renderRows = () => {
+        const keysToExclude = ["testConfigA", "timeSeries", "id"];
+        const keys = Object.keys(workload).filter((key) => !keysToExclude.includes(key));
+
+        return keys.map((key) => (
+            <tr key={key}>
+                <td className="p-3 ">{key}</td>
+                <td className="p-3">{workload[key]}</td>
+            </tr>
+        ));
+    };
     return (
         <div className="container mt-5">
-            <h1 className="mb-4">Workload A Data</h1>
-            <div className="card-body">
-                <p className="card-text">
-                    <strong className="card-title">Ops Per Sec: </strong>
-                    {workload.opsPerSec}
-                    <br />
-                    <strong>Read Mean Latency:</strong> {workload.readMeanLatency}
-                    <br />
-                    <strong>Read Max Latency:</strong> {workload.readMaxLatency}
-                    <br />
-                    <strong>Read P95:</strong> {workload.readP95}
-                    <br />
-                    <strong>Read P99:</strong> {workload.readP99}
-                    <br />
-                    <strong>Update Mean Latency:</strong> {workload.updateMeanLatency}
-                    <br />
-                    <strong>Update Max Latency:</strong> {workload.updateMaxLatency}
-                    <br />
-                    <strong>Update P95:</strong> {workload.updateP95}
-                    <br />
-                    <strong>Update P99:</strong> {workload.updateP99}
-                </p>
-                <p className="card-text">
-                    <strong>Database Option:</strong> {workload.testConfigA.dbConfig.databaseOption.database}
-                    <br />
-                    <strong>Platform:</strong> {workload.testConfigA.dbConfig.platform}
-                    <br />
-                    <strong>Number of Nodes:</strong> {workload.testConfigA.dbConfig.numOfNodes}
-                    <br />
-                    <strong>Number of Regions:</strong> {workload.testConfigA.dbConfig.numOfRegions}
-                    <br />
-                    <strong>Transactional:</strong> {workload.testConfigA.dbConfig.transactional ? "Yes" : "No"}
-                    <br />
-                    <strong>Multi-Region:</strong> {workload.testConfigA.dbConfig.multiRegion ? "Yes" : "No"}
-                </p>
-                <button className="btn btn-primary" onClick={goBack}>
+            <h1 className="mb-4">Workload A Data for {database}</h1>
+            <table className="table table-striped table-hover text-center w-auto ">
+                <thead>
+                    <tr>
+                        <th>Metric</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>{renderRows()}</tbody>
+            </table>
+            <div className="button-box">
+                <button className="btn btn-primary mt-3" onClick={downloadCSV}>
+                    Download CSV
+                </button>
+                <button className="btn btn-secondary ml-2 mt-3" onClick={goBack}>
                     Go Back
                 </button>
             </div>
