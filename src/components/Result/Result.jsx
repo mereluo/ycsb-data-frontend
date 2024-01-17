6f1fcf08bb040b55ecf696c1651350585925dc21
mereluo <meredithluo710@gmail.com>
2024-01-17 10:18:39 -0800

    Add line chart for time series

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./result.css";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 function Result() {
     const location = useLocation();
@@ -8,12 +12,15 @@ function Result() {
     const workload = location.state.workload;
     const database = workload.testConfigA.dbConfig.databaseOption.database;
     console.log(workload);
+
     const goBack = () => {
         navigateTo("/start");
     };
+
     const downloadCSV = () => {
         // Add logic to download data as CSV
     };
+
     const renderRows = () => {
         const keysToExclude = ["testConfigA", "timeSeries", "id"];
         const keys = Object.keys(workload).filter((key) => !keysToExclude.includes(key));
@@ -25,18 +32,69 @@ function Result() {
             </tr>
         ));
     };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: "top",
+            },
+            title: {
+                display: true,
+                text: "Time Series Line Chart",
+            },
+        },
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: "Time elapsed (milliseconds)",
+                    offset: true,
+                },
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: "Mean performance time (milliseconds)",
+                    offset: true,
+                },
+            },
+        },
+    };
+
+    const data = {
+        labels: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000],
+        datasets: [
+            {
+                label: "Mean READ Latency",
+                data: [32612, 27861, 28008, 27136, 27137, 27138, 27169, 26761, 26313, 25315],
+                borderColor: "rgba(75,192,192,1)",
+            },
+            {
+                label: "Mean Update Latency",
+                data: [66708, 55467, 51148, 49795, 50543, 49862, 49439, 49592, 48217, 48885],
+                borderColor: "#742774",
+            },
+        ],
+    };
+
     return (
         <div className="container mt-5">
             <h1 className="mb-4">Workload A Data for {database}</h1>
-            <table className="table table-striped table-hover text-center w-auto ">
-                <thead>
-                    <tr>
-                        <th>Metric</th>
-                        <th>Value</th>
-                    </tr>
-                </thead>
-                <tbody>{renderRows()}</tbody>
-            </table>
+            <div className="statistics">
+                <table className="table table-striped text-center w-auto mr-4">
+                    <thead>
+                        <tr>
+                            <th>Metric</th>
+                            <th>Value</th>
+                        </tr>
+                    </thead>
+                    <tbody>{renderRows()}</tbody>
+                </table>
+                <div className="graph-box">
+                    <Line options={options} data={data} />
+                </div>
+            </div>
             <div className="button-box">
                 <button className="btn btn-primary mt-3" onClick={downloadCSV}>
                     Download CSV
