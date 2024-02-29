869fecc9f0f52101a8f6d3a0988a34342b349379
mereluo <meredithluo710@gmail.com>
2024-02-29 10:54:35 -0800

    Reformat result page

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,148 +1,68 @@
-import React from "react";
-import "./result.css";
-import { Chart as ChartJS, LineElement, PointElement, Filler, LinearScale, Tooltip, Legend, Title } from "chart.js";
-import { Scatter } from "react-chartjs-2";
-ChartJS.register(LineElement, PointElement, Filler, LinearScale, Tooltip, Legend, Title);
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Table from "./Statistics/Table.jsx";
+import Graph from "./Statistics/Graph.jsx";
 
-function Result({ workload }) {
-    console.log(workload);
-    const timeSeries = workload.timeSeries;
-    console.log(timeSeries);
+function ResultMain() {
+    const location = useLocation();
+    const workloads = location.state.workload;
+    const navigateTo = useNavigate();
+    const [showData, setShowData] = useState(Array(workloads.length).fill(false));
 
-    const downloadCSV = () => {
-        let csvContent = "category,time,mean_latency\n";
-
-        Object.keys(timeSeries).forEach((entryKey) => {
-            const entry = timeSeries[entryKey];
-
-            entry.latency.forEach((latency, index) => {
-                const time = entry.time[index];
-
-                csvContent += `${entryKey},${time},${latency}\n`;
-            });
-        });
-
-        const blob = new Blob([csvContent], { type: "text/csv" });
-
-        const link = document.createElement("a");
-        link.href = URL.createObjectURL(blob);
-        link.download = "timeSeries.csv";
-        link.click();
+    const goBack = () => {
+        navigateTo("/search");
     };
 
-    const renderRows = () => {
-        const keys = Object.keys(workload.userDefinedFields);
-
-        return keys.map((key) => (
-            <tr key={key}>
-                <td className="pt-2 pb-2">{key}</td>
-                <td className="pt-2 pb-2">{workload.userDefinedFields[key]}</td>
-            </tr>
-        ));
+    const toggleData = (index) => {
+        const updateShowData = [...showData];
+        updateShowData[index] = !updateShowData[index];
+        setShowData(updateShowData);
     };
 
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                display: true,
-                position: "top",
-            },
-            title: {
-                display: true,
-                text: "Time Series",
-            },
-            tooltips: {
-                mode: "index",
-                intersect: false,
-            },
-        },
-        hover: {
-            mode: "nearest",
-            intersect: false,
-        },
-        scales: {
-            x: {
-                title: {
-                    display: true,
-                    text: "Time elapsed (milliseconds)",
-                },
-                beginAtZero: true,
-            },
-            y: {
-                title: {
-                    display: true,
-                    text: "Mean performance time (milliseconds)",
-                },
-                beginAtZero: true,
-            },
-        },
-        elements: {
-            line: {
-                tension: 0.4,
-            },
-        },
-    };
-
-    function getRandomColor() {
-        const letters = "0123456789ABCDEF";
-        let color = "#";
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-
-    const buildData = () => {
-        if (timeSeries !== null) {
-            return Object.keys(timeSeries).map((entryKey) => {
-                const entry = timeSeries[entryKey];
-                const label = `Mean ${entryKey.toUpperCase()} Latency`;
-                const transformedData = entry.time.map((value, index) => ({
-                    x: value,
-                    y: entry.latency[index],
-                }));
-                return {
-                    label,
-                    data: transformedData,
-                    showLine: true,
-                    fill: false,
-                    borderColor: getRandomColor(),
-                };
-            });
-        }
-        return null;
-    };
-
-    const data = {
-        datasets: buildData(),
-    };
-
+    console.log(location.state.workload);
     return (
-        <div className="result-container">
-            <div className="statistics">
-                <table className="table table-hover table-sm mr-4 text-center">
-                    <thead className="thead-light">
-                        <tr>
-                            <th>Metric</th>
-                            <th>Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>{renderRows()}</tbody>
-                </table>
-                {(timeSeries && (
-                    <div className="graph-box">
-                        <Scatter options={options} data={data} />
+        <div>
+            <div className="mt-4 ml-4">
+                {workloads.map((workload, index) => (
+                    <div key={index} className="workload-container d-flex flex-lg-row ">
+                        <div className="configuration mr-4">
+                            <h4>
+                                {index + 1}. Workload {workload.workloadType} for {workload.database}
+                            </h4>
+                            <p>
+                                <div className="card" style={{ width: "25rem" }}>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Configurations:</h5>
+                                        <p className="card-text">
+                                            <strong>Database:</strong> {workload.database}&nbsp;&nbsp;
+                                            <strong>Type:</strong> {workload.type} <br />
+                                            <strong>Workload Type:</strong> {workload.workloadType} &nbsp;&nbsp;
+                                            <strong>Update By:</strong> {workload.updateType} <br />
+                                            <strong>Concurrency Level:</strong> {workload.concurrencyLevel} <br />
+                                            <strong>Record Counts:</strong> {workload.recordCounts} million
+                                            <br />
+                                            <strong>Number of Nodes:</strong> {workload.numOfNodes} <br />
+                                            <strong>Multi-Regional:</strong> {workload.isMultiRegional} <br />
+                                        </p>
+                                    </div>
+                                    <button className="btn btn-outline-info btn-sm" onClick={() => toggleData(index)}>
+                                        {showData[index] ? "Hide Data" : "Show Data"}
+                                    </button>
+                                </div>
+                            </p>
+                        </div>
+                        <div className="d-flex flex-column">
+                            <div className="ml-3">{showData[index] && <Table workload={workload} />}</div>
+                            <div className="ml-3">{showData[index] && <Graph workload={workload} />}</div>
+                        </div>
                     </div>
-                )) || <div>No Time Series Data Available</div>}
-            </div>
-            <div className="button-box">
-                <button className="btn btn-primary btn-sm mt-3 mb-3" onClick={downloadCSV} disabled={timeSeries === null}>
-                    Download Time Series CSV
+                ))}
+                <button className="btn btn-outline-primary mt-3" onClick={goBack}>
+                    Start Another Search
                 </button>
             </div>
         </div>
     );
 }
 
-export default Result;
+export default ResultMain;
