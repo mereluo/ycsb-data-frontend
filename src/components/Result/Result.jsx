793e48a1097c17f44ea1d1b25efa6fa230f8659e
mereluo <meredithluo710@gmail.com>
2024-03-11 11:39:32 -0700

    Add delete to result page, fix bugs

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -9,14 +9,15 @@ import "./result.css";
 
 function Result() {
     const location = useLocation();
-    const workloads = location.state.workload;
+    let initialWorkloads = location.state.workload || [];
     const navigateTo = useNavigate();
-    const [showData, setShowData] = useState(Array(workloads.length).fill(false));
+    const [workloads, setWorkloads] = useState(initialWorkloads);
+    const [showData, setShowData] = useState(Array(initialWorkloads.length).fill(false));
 
     useEffect(() => {
-        // Your logic here (if needed) when showData changes
-        // This will run after each render when showData changes
-    }, [showData]);
+        // Update workloads when the location state changes
+        setShowData(Array(workloads).fill(false));
+    }, [workloads]);
 
     // console.log(location.state.workload);
     const goBack = () => {
@@ -31,6 +32,28 @@ function Result() {
         });
     };
 
+    const handleDelete = async (index) => {
+        const deleteId = workloads[index].id;
+        console.log(deleteId);
+        try {
+            const response = await fetch(`http://localhost:8080/api/workload/delete/${deleteId}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            if (response.ok) {
+                console.log("Delete Success");
+                setWorkloads((prevWorkloads) => prevWorkloads.filter((_, i) => i !== index));
+            } else {
+                const data = await response.json();
+                console.log(`Error: ${data.message}`);
+            }
+        } catch (error) {
+            console.error("Error deleting workload:", error);
+        }
+    };
+
     const columns = React.useMemo(
         () => [
             { Header: "Index", accessor: (row, index) => index + 1 },
@@ -42,16 +65,25 @@ function Result() {
             { Header: "Record Counts", accessor: "recordCounts" },
             { Header: "Number of Nodes", accessor: "numOfNodes" },
             { Header: "Multi-regional", accessor: "isMultiRegional" },
+            { Header: "Co-located", accessor: "isCoLocated" },
             {
                 Header: "Details",
                 accessor: (row, index) => (
                     <button className="btn btn-outline-info btn-sm" onClick={() => toggleData(index)}>
-                        {showData[index] ? "Hide Data" : "Show Data"}
+                        {showData[index] ? "Hide" : "Show"}
+                    </button>
+                ),
+            },
+            {
+                Header: "Delete",
+                accessor: (row, index) => (
+                    <button className="btn btn-outline-danger btn-sm" onClick={() => handleDelete(index)}>
+                        Delete
                     </button>
                 ),
             },
         ],
-        [showData]
+        [showData, workloads]
     );
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data: workloads }, useSortBy);
 
@@ -90,10 +122,14 @@ function Result() {
             </button>
             <div className="d-flex flex-wrap mt-3">
                 {workloads.map((workload, index) => (
-                    <div key={index} className="ml-3 mb-3">
-                        {showData[index] && <h5>Table for {index + 1}</h5>}
-                        {showData[index] && <Table workload={workload} />}
-                        {showData[index] && <Graph workload={workload} />}
+                    <div key={index} className="">
+                        {showData[index] && (
+                            <div>
+                                <h5>Table for {index + 1}</h5>
+                                <Table workload={workload} />
+                                <Graph workload={workload} />
+                            </div>
+                        )}
                     </div>
                 ))}
             </div>
