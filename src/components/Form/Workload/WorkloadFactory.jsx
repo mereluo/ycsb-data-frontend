912e7028ec7b4fe380320cad50f27bc1ac95bc7a
mereluo <meredithluo710@gmail.com>
2024-03-28 13:42:45 -0700

    Add templates for ycsb-r

--- src/components/Form/Workload/WorkloadFactory.jsx
+++ src/components/Form/Workload/WorkloadFactory.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import { AB_T, AB, F, F_T } from "../../../models/Templates";
+import { AB, AB_T, F, F_T, G, C, G_T, C_T } from "../../../models/Templates";
 import Workload from "./Workload";
 
 function WorkloadFactory({ type, test }) {
     const selectTemplate = (type, test) => {
-        if (test === "ycsb") {
+        if (test === "ycsb" || test === "ycsb-r") {
             if (type === "A" || type === "B") return AB;
             if (type === "F") return F;
+            if (type === "G") return G;
+            if (type === "C") return C;
         }
         if (test === "ycsb-t") {
             if (type === "A" || type === "B") return AB_T;
             if (type === "F") return F_T;
+            if (type === "G") return G_T;
+            if (type === "C") return C_T;
         }
     };
     return <Workload metric={selectTemplate(type, test)} />;
